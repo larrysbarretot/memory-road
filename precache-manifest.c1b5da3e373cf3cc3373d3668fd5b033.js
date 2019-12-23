2b47ded7933c1c13b1d3e465f14e5ce3dda36086
Larry Steve Barreto Trujillo <larrysbarretot@gmail.com>
2019-12-23 17:43:15 -0500

    deploy

---
+++ precache-manifest.c1b5da3e373cf3cc3373d3668fd5b033.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4358978363b173090ecb",
+    "url": "/memory-road/css/app.0eef4007.css"
+  },
+  {
+    "revision": "45bd110340a6c2e2453c",
+    "url": "/memory-road/css/chunk-vendors.504464cf.css"
+  },
+  {
+    "revision": "e8b466684749b17a5b931f2366d19b74",
+    "url": "/memory-road/img/dory.e8b46668.png"
+  },
+  {
+    "revision": "e274064fdd1d5e32879b8580091dbd34",
+    "url": "/memory-road/img/lab-logo.e274064f.png"
+  },
+  {
+    "revision": "49003bafb6aa59326de442b5ee485a39",
+    "url": "/memory-road/index.html"
+  },
+  {
+    "revision": "4358978363b173090ecb",
+    "url": "/memory-road/js/app.b725b2f7.js"
+  },
+  {
+    "revision": "45bd110340a6c2e2453c",
+    "url": "/memory-road/js/chunk-vendors.2f2f49f4.js"
+  },
+  {
+    "revision": "8df08095fa8ff3c530511c0ada812af6",
+    "url": "/memory-road/manifest.json"
+  },
+  {
+    "revision": "0544f48b7b0a68c47d042cebd32b4f21",
+    "url": "/memory-road/media/background.0544f48b.mp3"
+  },
+  {
+    "revision": "e8fedebae982db26b2778587c33d42f0",
+    "url": "/memory-road/media/correct.e8fedeba.mp3"
+  },
+  {
+    "revision": "e750c3441a2acdf1baebc19ea9bf2d80",
+    "url": "/memory-road/media/error1.e750c344.mp3"
+  },
+  {
+    "revision": "3cdede2db5a2c001315d6c479d71a7c7",
+    "url": "/memory-road/media/error10.3cdede2d.mp3"
+  },
+  {
+    "revision": "61562c30fe4d43be64c13943be5b83e4",
+    "url": "/memory-road/media/error11.61562c30.mp3"
+  },
+  {
+    "revision": "d0acff9492510f557de252809433a576",
+    "url": "/memory-road/media/error12.d0acff94.mp3"
+  },
+  {
+    "revision": "9f3f5de8c168ed5dd0c6d84dc1f978d5",
+    "url": "/memory-road/media/error2.9f3f5de8.mp3"
+  },
+  {
+    "revision": "6b4693848b83bf69097e4dd24296db8b",
+    "url": "/memory-road/media/error3.6b469384.mp3"
+  },
+  {
+    "revision": "09700db64c8da1c95a898c02a44262f1",
+    "url": "/memory-road/media/error4.09700db6.mp3"
+  },
+  {
+    "revision": "59389d609b746d72e6a8e4795ead160a",
+    "url": "/memory-road/media/error5.59389d60.mp3"
+  },
+  {
+    "revision": "3e98866bf469bd932b22243bef18fd17",
+    "url": "/memory-road/media/error6.3e98866b.mp3"
+  },
+  {
+    "revision": "e186528cba0b29dd24a2c60b0c78fee3",
+    "url": "/memory-road/media/error7.e186528c.mp3"
+  },
+  {
+    "revision": "e294a9075d70f2f8db6313b52f36009d",
+    "url": "/memory-road/media/error8.e294a907.mp3"
+  },
+  {
+    "revision": "46e4e1700d54c9a54908c9ab515bd179",
+    "url": "/memory-road/media/error9.46e4e170.mp3"
+  },
+  {
+    "revision": "2c42fa9a15d157b01f1ba922c82f8f8b",
+    "url": "/memory-road/media/win1.2c42fa9a.mp3"
+  },
+  {
+    "revision": "2564d6b46d3ea6b2cbc895a9d3d19a9b",
+    "url": "/memory-road/media/win2.2564d6b4.mp3"
+  },
+  {
+    "revision": "dc8916623a70611fbb5ecefda66020f3",
+    "url": "/memory-road/media/win3.dc891662.mp3"
+  },
+  {
+    "revision": "694b0c2996f6d7ce6eb297f633f42da6",
+    "url": "/memory-road/media/youwin.694b0c29.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/memory-road/robots.txt"
+  }
+]);
